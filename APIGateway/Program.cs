61f2c326441faeb3d5c62993174bfe3b282517be
suvidhure <suvidya2@gmail.com>
2019-01-27 22:48:56 +0200

    Add project files.

---
+++ APIGateway/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args);
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                .ConfigureAppConfiguration(ic => ic.AddJsonFile(Path.Combine("configuration", "configuration.json")))
+                .UseStartup<Startup>();
+            IWebHost host = builder.Build();
+            return host;
+        }
+
+
+    }
+}
---
+++ CustomerAPIServices/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Services.CustomerAPIServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Catcher Wong", "James Li" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Catcher Wong - {id}";
+        }
+    }
+}
